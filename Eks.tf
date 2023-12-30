af147edf7c998c98ed9f7f3d3a15e3dc28777b80
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-12-30 21:13:01 +0530

    Create Eks.tf

---
+++ Eks.tf
@@ -0,0 +1,122 @@
+resource "aws_iam_role" "eks-iam-role" {
+ name = "kubernetes-eks-iam-role"
+
+ path = "/"
+
+ assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
+}
+
+resource "aws_eks_cluster" "kubernetes-eks" {
+ name = "eks-cluster"
+ role_arn = aws_iam_role.eks-iam-role.arn
+
+ vpc_config {
+  subnet_ids = [aws_subnet.pubsub1.id, aws_subnet.pubsub2.id]
+ }
+
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
+}
+
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
+
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+}
+
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.kubernetes-eks.name
+  node_group_name = "eks-workernodes"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids   = [aws_subnet.pubsub1.id, aws_subnet.pubsub2.id]
+  instance_types = ["t2.micro"]
+   
+  scaling_config {
+   desired_size = 2
+   max_size   = 2
+   min_size   = 2
+  }
+   
+   lifecycle {
+    prevent_destroy = false
+  }
+  capacity_type = "ON_DEMAND"
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "null_resource" "install_kubectl" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+      sudo apt-get install unzip
+      unzip awscliv2.zip
+      sudo ./aws/install
+      curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+      sudo chmod +x kubectl
+      sudo mv kubectl /usr/local/bin/kubectl
+
+      aws eks update-kubeconfig --region us-east-1 --name eks-cluster
+
+    EOT
+  }
+}
